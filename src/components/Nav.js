aaa5725b6b8e065bc2759d0515bf4f6e9d79e8ce
Olivers11 <olstertecn597@outlook.com>
2021-10-27 15:14:41 -0600

    navbar customizing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -22,25 +22,35 @@ export default function Nav() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">
-                            <li className="nav-item active">
-                                <Link to="/" className="nav-link">Notes</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/create" className="nav-link">Create Note</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/user" className="nav-link">Create User</Link>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Misiones
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a className="dropdown-item" >Mision del Norte</a>
+                                    <a className="dropdown-item" >Mision del Sur</a>
+                                    <a className="dropdown-item" >Mision del Altiplano</a>
+                                    <a className="dropdown-item" >Mision de Oriente</a>
+                                    <a className="dropdown-item" >Mision del Lago</a>
+                                </div>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown
+                                Asociaciones
                                 </a>
                                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a className="dropdown-item" >Action</a>
-                                    <a className="dropdown-item" >Another action</a>
-                                    <a className="dropdown-item" >Something else here</a>
+                                    <a className="dropdown-item" >Asociacion Central</a>
+                                    <a className="dropdown-item" >Asociacion Metropolitana</a>
+                                    <a className="dropdown-item" >Asociacion Occidental</a>
                                 </div>
                             </li>
+                            <li className="nav-item active">
+                                <Link to="/Informacion" className="nav-link">Informacion</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/Contact" className="nav-link">Contacto</Link>
+                            </li>
+                            
                         </ul>
                     </div>
                 </div>
