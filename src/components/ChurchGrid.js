3ebc2fd7ae988d1bb5a92fa044565a28bf35ef5b
Olivers11 <olstertecn597@outlook.com>
2021-11-07 14:17:33 -0600

    showing well the churches for lower distance

--- src/components/ChurchGrid.js
+++ src/components/ChurchGrid.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
-
+import { calcCrow } from "../helpers/Distance";
+import { getDistance } from "../helpers/Distance";
 
 export default function ChurchGrid() {
 
+
     const [element, setElement] = useState([]);
     const [pos, setPos] = useState([]);
-
     useEffect(() =>{
         getData();
         navigator.geolocation.getCurrentPosition((pos) =>{
-            console.log('Latitud: ', pos.coords.latitude);
-            console.log('Latitud: ', pos.coords.longitude);
+            //console.log('Latitud: ', pos.coords.latitude);
+            //console.log('Latitud: ', pos.coords.longitude);
             var myPos = [pos.coords.latitude, pos.coords.longitude];
             setPos(myPos);
         });
@@ -20,7 +21,6 @@ export default function ChurchGrid() {
     const getData = async() =>{
         const data = await fetch("https://pure-gorge-01437.herokuapp.com/getChurch/");
         const churchs = await data.json();
-        console.log(churchs);
         setElement(churchs);
     }
 
@@ -30,16 +30,22 @@ export default function ChurchGrid() {
             <h2>Iglesias</h2>
             <ul>
                 {
-                    element.map(item =>(
-                        <div key={item.Id + item.Nombre[1]} className="card p-2 mt-4">
-                            <h3 className="card-title">{item.Nombre}</h3>
-                            <p className="card-text">{item.Ubicacion}</p>
-                            <iframe src={`https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d30880.08117208651!2d${pos[1]}!3d${pos[0]}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!3e6!4m3!3m2!1d${pos[0]}!2d${pos[1]}!4m3!3m2!1d${item.PosX}!2d${item.PosY}!5e0!3m2!1ses!2sgt!4v1635305813144!5m2!1ses!2sgt`} width="600" height="450"   loading="lazy"></iframe>
-
-                        </div>   
-
-
-                    ))
+                    element.map(item =>{
+                        if(getDistance(element, item, pos))
+                        {
+                            return(
+                                <div key={item.Id + item.Nombre[1]} className="card p-2 mt-4">
+                                    <div className="card-body mx-auto">
+                                        <h3 className="card-title">{item.Nombre}</h3>
+                                        <p className="card-text">{item.Ubicacion}</p>
+                                        <iframe src={`https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d30880.08117208651!2d${pos[1]}!3d${pos[0]}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!3e6!4m3!3m2!1d${pos[0]}!2d${pos[1]}!4m3!3m2!1d${item.PosX}!2d${item.PosY}!5e0!3m2!1ses!2sgt!4v1635305813144!5m2!1ses!2sgt`} width="600" height="450"   loading="lazy"></iframe>
+                                        <p>{calcCrow(pos[0], pos[1], item.PosX, item.PosY)} Km</p>
+                                    </div>
+                                </div>
+                            )
+                        }
+                        // console.log(getDistance(element, item, pos));
+                    })
                 }
             </ul>
         </div>
---
+++ src/helpers/Distance.js
@@ -0,0 +1,64 @@
+function calcCrow(lat1, lon1, lat2, lon2) {
+    var R = 6371; // km
+    var dLat = toRad(lat2 - lat1);
+    var dLon = toRad(lon2 - lon1);
+    var lat1 = toRad(lat1);
+    var lat2 = toRad(lat2);
+
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) *
+            Math.sin(dLon / 2) *
+            Math.cos(lat1) *
+            Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return Number.parseFloat(d).toFixed(2);;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) {
+    return (Value * Math.PI) / 180;
+}
+
+function getDistance(elements, item, current, me){
+    let arr = [];
+    let lowers = [];
+    for(var j = 0; j < elements.length; j++){
+        var i = elements[j];
+        arr.push(Number.parseFloat(calcCrow(current[0], current[1], i.PosX, i.PosY)));
+    }   
+    var cont = 0;
+    while(cont < 5){
+        const minor = Math.min.apply(null, arr);
+        lowers.push(Number.parseFloat(minor));
+        const pos = arr.indexOf(minor);
+        if(pos != -1){
+            arr.splice(pos, 1);
+        }
+        cont += 1;
+    }
+
+    // console.log(lowers);
+    for(var i = 0; i < 5; i++){
+        //console.log(`coord: ${calcCrow(current[0], current[1], item.PosX, item.PosY).toString()}`);
+        if(calcCrow(current[0], current[1], item.PosX, item.PosY).toString() == lowers[i].toString()){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+
+module.exports = {
+    calcCrow,
+    toRad,
+    getDistance
+}
+
+
+
+
