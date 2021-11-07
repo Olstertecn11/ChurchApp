e44ee8255912078e4cb4210e7a9a0adcd9b80ce3
Olivers11 <olstertecn597@outlook.com>
2021-11-07 17:11:54 -0600

    modify for movil devices2

--- src/components/Asociacion.js
+++ src/components/Asociacion.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./style-components/Asociacion.css";
 
 export default function Asociacion() {
     return (
-        <div>
-            Asociaciones
-        </div>
-    )
+        <>
+            <div className="container">
+                <div className="row">
+                    <h3 className="text-center">Asociaciones</h3>
+
+                    <div className="form-group mx-auto mt-4">
+                        <select
+                            className="form-control mx-auto"
+                            id="exampleFormControlSelect1"
+                        >
+                            <option>Asociacion Central</option>
+                            <option>Asociacion Metropolitana</option>
+                            <option>Asociacion Occidental</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
