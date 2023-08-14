29eec7451ce0da9e6b20360f1595d3c7fae3fe20
Olstertecn11 <otzunund@miumg.edu.gt>
2023-08-13 18:45:30 -0600

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,11 @@ import { Routes, Route } from 'react-router-dom';
 // import UniqueChurch from './components/UniqueChurch';
 
 
-const Home = ()=>{
-	return(<h1>Home</h1>);
-}
-
-const SearchPage = ()=>{
-	return(<h1>SearchPage:</h1>);
-}
-
-
-
 export default function App() {
 	return (
 		<div className='App'>
 			<Routes>
 				<Route path='/' element={<Main />} />
-				<Route path='/search-page' element={<SearchPage />} />
 			</Routes>
 		</div>
 
